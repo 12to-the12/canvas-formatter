cb8dbd03733faf958f75e23d2c5adec89b5390fe
Logan Hillyer <shivastranded@proton.me>
2024-09-03 10:17:05 -0700

    brew

--- mac-install.sh
+++ mac-install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "Installing the brew package manager.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/bin
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing pandoc..."
 brew install pandoc
 
 echo "Building virtual environment..."
-python -m venv venv
+python3 -m venv venv
 
 echo "Upgrading venv..."
-./venv/bin/python -m pip install --upgrade pip
+./venv/bin/python3 -m pip install --upgrade pip
 
 echo "Installing packages..."
-./venv/bin/python -m pip install -r ./requirements.txt
+./venv/bin/python3 -m pip install -r ./requirements.txt
